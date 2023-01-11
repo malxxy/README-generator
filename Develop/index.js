7b67f8f20c30f1d1fb670035f4c87abf8e84a5b7
malxxy <maliayraguen@gmail.com>
2023-01-11 15:58:28 -0800

    inquirer prompts

--- Develop/index.js
+++ Develop/index.js
@@ -12,6 +12,45 @@ const questions = [
     // Questions
 ];
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Describe your project in a sentence or two.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: "What are te=he instructions for installation?",
+      name: 'installation',
+    },
+    {
+      type: 'checkbox',
+      choices: ['MIT','etc','letter'],
+      message: 'What license?',
+      name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Who besdies you contributed to this project? If not, please enter "N/A".',
+        name: 'contributors',
+    },
+    {
+        type: 'input',
+        message: 'What tests need to be run?',
+        name: 'tests',
+    },
+  ])
+  .then((data) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
